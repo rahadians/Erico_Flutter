bc2e5421b60f82f304a9b91321078598d4162f83
rahadians <matrix.black82@gmail.com>
2021-08-26 18:51:18 +0700

    http get api

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:day36_postapi/post_result_model.dart';
+import 'package:day36_postapi/user_model.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,6 +15,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   PostResult postResult = null;
+  User user = null;
 
   @override
   Widget build(BuildContext context) {
@@ -27,25 +29,29 @@ class _MyAppState extends State<MyApp> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Text(
-
-              (postResult != null)
-                  ? postResult.id +
-                      " | " +
-                      postResult.name +
-                      " | " +
-                      postResult.job +
-                      " | " +
-                      postResult.created
-                  : "Data Tidak Ada",
-             ),
-
+                // (postResult != null)
+                //     ? postResult.id +
+                //         " | " +
+                //         postResult.name +
+                //         " | " +
+                //         postResult.job +
+                //         " | " +
+                //         postResult.created
+                //     : "Data Tidak Ada",
+                (user != null)
+                    ? user.id + " | " + user.email
+                    : "Tidak ada data"),
             ElevatedButton(
                 onPressed: () {
-                  PostResult.connectToAPI("Badu", "Dokter")
-                      .then((value) => postResult = value);
-                  setState(() {});
+                  // PostResult.connectToAPI("Badu", "Dokter")
+                  //     .then((value) => postResult = value);
+                  // setState(() {});
+                  User.connectToAPI("2").then((value) => user=value);
+                  setState(() {
+
+                  });
                 },
-                child: Text("POST")),
+                child: Text("GET")),
           ],
         ),
       ),
---
+++ lib/user_model.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+
+class User {
+  String id;
+  String email;
+
+  User({this.id, this.email});
+
+  factory User.createUser(Map<String, dynamic> object) {
+    return User(id: object['id'].toString(),
+        email: object['email']);
+  }
+
+  static Future<User> connectToAPI(String id) async
+  {
+    // final apiURL ="https://reqres.in/api/users/" + id;
+    final apiURL = Uri.parse("https://reqres.in/api/users/"+ id);
+
+    var apiResult = await http.get(apiURL);
+    if (apiResult.statusCode==200){
+      print('konek ok');
+    }else{
+      print('konek gagal');
+    }
+    var jsonObject = json.decode(apiResult.body);
+    var userData =(jsonObject as Map<String,dynamic>)['data'];
+    return User.createUser(userData);
+
+  }
+
+}
